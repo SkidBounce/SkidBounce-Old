6d087d658ae46825ba13daf3ef2eb96ad0910c44
ManInMyVan <maninmyvan@gmail.com>
2024-01-14 20:16:49 -0700

    update window title slightly

--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -57,11 +57,11 @@ object LiquidBounce {
     val clientVersionNumber = clientVersionText.substring(1).toIntOrNull() ?: 0 // version format: "b<VERSION>" on legacy
     val clientCommit = gitInfo["git.commit.id.abbrev"]?.let { "git-$it" } ?: "unknown"
     val clientBranch = gitInfo["git.branch"]?.toString() ?: "unknown"
-    const val IN_DEV = false
+    const val IN_DEV = true
     const val CLIENT_CREATOR = "CCBlueX"
     const val CLIENT_CLOUD = "https://cloud.liquidbounce.net/LiquidBounce"
 
-    val clientTitle = CLIENT_NAME + " " + clientVersionText + " " + clientCommit + if (IN_DEV) " | DEVELOPMENT BUILD" else ""
+    val clientTitle = "$CLIENT_NAME $clientVersionText ${if (IN_DEV) clientCommit else ""}"
 
     var isStarting = true
 
