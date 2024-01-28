75e60457a08f802472b14aceac84fa748a3a32a8
ManInMyVan <maninmyvan@gmail.com>
2024-01-28 12:29:20 -0700

    540b78e
    I did it out of order for some reason

---
+++ src/main/java/net/ccbluex/liquidbounce/utils/PlayerSimulation.kt
@@ -0,0 +1,14 @@
+/*
+ * SkidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge, Forked from LiquidBounce.
+ * https://github.com/ManInMyVan/SkidBounce/
+ */
+package net.ccbluex.liquidbounce.utils
+
+import net.minecraft.util.Vec3
+
+interface PlayerSimulation {
+    val pos: Vec3
+
+    fun tick()
+}
