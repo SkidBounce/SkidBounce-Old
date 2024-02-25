aa759bc568febd404652e538b6fcab8743f78d7b
ManInMyVan <maninmyvan@gmail.com>
2024-02-25 15:11:08 -0700

    63f5894

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/AimBot.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/AimBot.kt
@@ -5,11 +5,8 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.combat
 
-import net.ccbluex.liquidbounce.event.EventState
-import net.ccbluex.liquidbounce.event.EventTarget
-import net.ccbluex.liquidbounce.event.MotionEvent
-import net.ccbluex.liquidbounce.features.module.Module
-import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.event.*
+import net.ccbluex.liquidbounce.features.module.*
 import net.ccbluex.liquidbounce.features.module.modules.player.Reach
 import net.ccbluex.liquidbounce.utils.EntityUtils.isSelected
 import net.ccbluex.liquidbounce.utils.RotationUtils.getRotationDifference
@@ -23,6 +20,7 @@ import net.ccbluex.liquidbounce.utils.timing.MSTimer
 import net.ccbluex.liquidbounce.value.*
 import net.minecraft.entity.Entity
 import java.util.*
+import kotlin.math.atan
 
 object AimBot : Module("AimBot", ModuleCategory.COMBAT) {
 
@@ -32,10 +30,14 @@ object AimBot : Module("AimBot", ModuleCategory.COMBAT) {
     private val predictClientMovement by IntegerValue("PredictClientMovement", 2, 0..5)
     private val predictEnemyPosition by FloatValue("PredictEnemyPosition", 1.5f, -1f..2f)
     private val fov by FloatValue("FOV", 180F, 1F..180F)
-    private val center by BoolValue("Center", false)
     private val lock by BoolValue("Lock", true)
     private val onClick by BoolValue("OnClick", false)
     private val jitter by BoolValue("Jitter", false)
+    private val yawJitterMultiplier by FloatValue("JitterYawMultiplier", 1f, 0.1f..2.5f)
+    private val pitchJitterMultiplier by FloatValue("JitterPitchMultiplier", 1f, 0.1f..2.5f)
+    private val center by BoolValue("Center", false)
+    private val headLock by BoolValue("Headlock", false) { center }
+    private val headLockBlockHeight by FloatValue("headBlockHeight", -1f, -2f..0f) { headLock }
 
     private val clickTimer = MSTimer()
 
@@ -84,11 +86,11 @@ object AimBot : Module("AimBot", ModuleCategory.COMBAT) {
         // Some players do jitter on their mouses causing them to shake around. This is trying to simulate this behavior.
         if (jitter) {
             if (random.nextBoolean()) {
-                thePlayer.fixedSensitivityYaw += (random.nextGaussian() - 0.5f).toFloat()
+                thePlayer.fixedSensitivityYaw += ((random.nextGaussian() - 0.5f) * yawJitterMultiplier).toFloat()
             }
 
             if (random.nextBoolean()) {
-                thePlayer.fixedSensitivityPitch += (random.nextGaussian() - 0.5f).toFloat()
+                thePlayer.fixedSensitivityPitch += ((random.nextGaussian() - 0.5f) * pitchJitterMultiplier).toFloat()
             }
         }
     }
@@ -127,8 +129,19 @@ object AimBot : Module("AimBot", ModuleCategory.COMBAT) {
 
         if (destinationRotation == null) {
             player.setPosAndPrevPos(currPos, oldPos)
-
             return false
+        }
+
+        // look headLockBlockHeight higher
+        if (headLock) {
+            val distance = player.getDistanceToEntityBox(entity)
+            val playerEyeHeight = player.eyeHeight
+            val blockHeight = headLockBlockHeight
+
+            // Calculate the pitch offset needed to shift the view one block up
+            val pitchOffset = Math.toDegrees(atan((blockHeight + playerEyeHeight) / distance)).toFloat()
+
+            destinationRotation.pitch -= pitchOffset
         }
 
         // Figure out the best turn speed suitable for the distance and configured turn speed
