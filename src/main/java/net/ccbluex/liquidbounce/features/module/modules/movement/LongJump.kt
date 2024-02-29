fade497d11af88e655da2da4f677f73e10284ebf
ManInMyVan <maninmyvan@gmail.com>
2024-02-28 19:55:41 -0700

    Add Redesky changes from old FDP commit
    and some other small changes

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/LongJump.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/LongJump.kt
@@ -29,7 +29,7 @@ object LongJump : Module("LongJump", ModuleCategory.MOVEMENT) {
         Mineplex, Mineplex2, Mineplex3,
 
         // Redesky
-        Redesky, Redesky2,
+        Redesky, Redesky2, Redesky3,
 
         // Other
         Hycraft, Buzz,
@@ -38,21 +38,33 @@ object LongJump : Module("LongJump", ModuleCategory.MOVEMENT) {
     private val modes = longJumpModes.map { it.modeName }.toTypedArray()
 
     val mode by ListValue("Mode", modes, "NCP")
-    val ncpBoost by FloatValue("NCP-Boost", 4.25f, 1f..10f) { mode == "NCP" }
-    val redeskyJumpMovement by FloatValue("Redesky-JumpMovement", 0.15f, 0.05f..0.25f) { mode == "Redesky" }
-    val redeskyMotionY by FloatValue("Redesky-MotionY",0.05f,0.05f..1f) { mode == "Redesky" }
-    val redeskyUseTimer by BoolValue("Redesky-UseTimer", false) { mode == "Redesky" }
-    val redeskyTimer by FloatValue("Redesky-Timer", 0.7f, 0.1f..1f) { mode == "Redesky" }
-    val redesky2YMotionReducer by BoolValue("Redesky2-YMotionReducer", false) { mode == "Redesky2" }
-    val redesky2YMotion by FloatValue("Redesky2YMotion", 0.08f, 0.01f..0.2f) { mode == "Redesky2" }
-    val redesky2ReduceYMotion by FloatValue("Redesky2-ReduceYMotion", 0.15f, 0.01f..0.2f) { mode == "Redesky2" }
-    val redesky2MinYMotion by FloatValue("Redesky2-MinYMotion", 0.04f, 0.01f..0.2f) { mode == "Redesky2" }
-    val redesky2AirSpeed by FloatValue("Redesky2-AirSpeed", 0.1f,0.05f..0.25f) { mode == "Redesky2" }
-    val redesky2MinAirSpeed by FloatValue("Redesky2-MinAirSpeed", 0.08f, 0.05f..0.25f) { mode == "Redesky2" }
-    val redesky2ReduceAirSpeed by FloatValue("Redesky2-ReduceAirSpeed", 0.16f,0.05f..0.25f) { mode == "Redesky2" }
-    val redesky2AirSpeedReducer by BoolValue("Redesky2-AirSpeedReducer", false) { mode == "Redesky2" }
 
-    private val autoJump by BoolValue("AutoJump", false)
+    val ncpBoost by FloatValue("NCP-Boost", 4.25f, 1f..10f) { mode == "NCP" }
+
+    val redeskyJumpMovement by FloatValue("Redesky-JumpMovement", 0.15f, 0.05f..0.25f) { mode == "Redesky" }
+    val redeskyMotionY by FloatValue("Redesky-MotionY", 0.5f, 0.05f..1f) { mode == "Redesky" }
+    val redeskyUseTimer by BoolValue("Redesky-UseTimer", false) { mode == "Redesky" }
+    val redeskyTimer by FloatValue("Redesky-Timer", 0.7f, 0.1f..1f) { mode == "Redesky" && redeskyUseTimer }
+
+    val redesky2YMotion by FloatValue("Redesky2YMotion", 0.08f, 0.01f..0.2f) { mode == "Redesky2" }
+    val redesky2YMotionReducer by BoolValue("Redesky2-YMotionReducer", false) { mode == "Redesky2" }
+    val redesky2ReduceYMotion by FloatValue("Redesky2-ReduceYMotion", 0.15f, 0.01f..0.2f) { mode == "Redesky2" && redesky2YMotionReducer }
+    val redesky2MinYMotion by FloatValue("Redesky2-MinYMotion", 0.04f, 0.01f..0.2f) { mode == "Redesky2" && redesky2YMotionReducer }
+    val redesky2AirSpeed by FloatValue("Redesky2-AirSpeed", 0.1f, 0.05f..0.25f) { mode == "Redesky2" }
+    val redesky2AirSpeedReducer by BoolValue("Redesky2-AirSpeedReducer", false) { mode == "Redesky2" }
+    val redesky2ReduceAirSpeed by FloatValue("Redesky2-ReduceAirSpeed", 0.16f, 0.05f..0.25f) { mode == "Redesky2" && redesky2AirSpeedReducer }
+    val redesky2MinAirSpeed by FloatValue("Redesky2-MinAirSpeed", 0.08f, 0.05f..0.25f) { mode == "Redesky2" && redesky2AirSpeedReducer }
+
+    val redesky3JumpMovement by FloatValue("Redesky3-JumpMovement", 0.13f, 0.05f..0.25f) { mode == "Redesky3" }
+    val redesky3MovementReducer by BoolValue("Redesky3-MovementReducer", true) { mode == "Redesky3" }
+    val redesky3ReduceMovement by FloatValue("Redesky3-ReduceMovement", 0.08f, 0.05f..0.25f) { mode == "Redesky3" && redesky3MovementReducer }
+    val redesky3MotionY by FloatValue("Redesky3-MotionY", 0.81f, 0.05f..1f) { mode == "Redesky3" }
+    val redesky3YMotionReducer by BoolValue("Redesky3-YMotionReducer", true) { mode == "Redesky3" }
+    val redesky3ReduceYMotion by FloatValue("Redesky3-ReduceYMotion", 0.12f, 0.01f..0.2f) { mode == "Redesky3" && redesky3YMotionReducer }
+    val redesky3UseTimer by BoolValue("Redesky3-UseTimer", true) { mode == "Redesky3" }
+    val redesky3Timer by FloatValue("Redesky3-Timer", 0.36f, 0.1f..1f) { mode == "Redesky3" && redesky3UseTimer }
+
+    private val autoJump by BoolValue("AutoJump", true)
 
     var jumped = false
     var canBoost = false
---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/longjumpmodes/other/Redesky3.kt
@@ -0,0 +1,42 @@
+/*
+ * SkidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge, Forked from LiquidBounce.
+ * https://github.com/ManInMyVan/SkidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement.longjumpmodes.other
+
+import net.ccbluex.liquidbounce.features.module.modules.movement.LongJump.redesky3MovementReducer
+import net.ccbluex.liquidbounce.features.module.modules.movement.LongJump.redesky3ReduceMovement
+import net.ccbluex.liquidbounce.features.module.modules.movement.LongJump.redesky3UseTimer
+import net.ccbluex.liquidbounce.features.module.modules.movement.LongJump.redesky3JumpMovement
+import net.ccbluex.liquidbounce.features.module.modules.movement.LongJump.redesky3MotionY
+import net.ccbluex.liquidbounce.features.module.modules.movement.LongJump.redesky3ReduceYMotion
+import net.ccbluex.liquidbounce.features.module.modules.movement.LongJump.redesky3Timer
+import net.ccbluex.liquidbounce.features.module.modules.movement.LongJump.redesky3YMotionReducer
+import net.ccbluex.liquidbounce.features.module.modules.movement.longjumpmodes.LongJumpMode
+import net.ccbluex.liquidbounce.utils.extensions.resetSpeed
+
+/**
+ * @author SkidderMC/FDPClient
+ * @author liulihaocai
+ */
+object Redesky3 : LongJumpMode("Redesky3") {
+    private var airTicks = 0
+    override fun onDisable() {
+        mc.timer.resetSpeed()
+        airTicks = 0
+    }
+    override fun onUpdate() {
+        if (mc.thePlayer.onGround) return
+
+        ++airTicks
+
+        val reduceMovement = if (redesky3MovementReducer) airTicks * redesky3ReduceMovement / 100f else 0f
+        mc.thePlayer.jumpMovementFactor = redesky3JumpMovement - reduceMovement
+
+        val reduceYMotion = if (redesky3YMotionReducer) airTicks * redesky3ReduceYMotion / 100f else 0f
+        mc.thePlayer.motionY += (redesky3MotionY / 10) - reduceYMotion
+
+        mc.timer.timerSpeed = if (redesky3UseTimer) redesky3Timer else 1f
+    }
+}
