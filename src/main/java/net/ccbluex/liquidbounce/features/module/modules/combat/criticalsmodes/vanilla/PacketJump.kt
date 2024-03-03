dcb4219c92d9f32e9c3eb2f5d0fe54bf294fe3a3
ManInMyVan <maninmyvan@gmail.com>
2024-03-02 19:29:40 -0700

    Port More & PacketJump from FDP

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/criticalsmodes/vanilla/PacketJump.kt
@@ -0,0 +1,31 @@
+/*
+ * SkidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge, Forked from LiquidBounce.
+ * https://github.com/ManInMyVan/SkidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.combat.criticalsmodes.vanilla
+
+import net.ccbluex.liquidbounce.features.module.modules.combat.Criticals.crit
+import net.ccbluex.liquidbounce.features.module.modules.combat.Criticals.sendPacket
+import net.ccbluex.liquidbounce.features.module.modules.combat.criticalsmodes.CriticalsMode
+import net.minecraft.entity.Entity
+
+/**
+ * @author SkidderMC/FDPClient
+ */
+object PacketJump : CriticalsMode("PacketJump") {
+    override fun onAttack(entity: Entity) {
+        sendPacket(0.41999998688698, false)
+        sendPacket(0.7531999805212, false)
+        sendPacket(1.00133597911214, false)
+        sendPacket(1.16610926093821, false)
+        sendPacket(1.24918707874468, false)
+        sendPacket(1.1707870772188, false)
+        sendPacket(1.0155550727022, false)
+        sendPacket(0.78502770378924, false)
+        sendPacket(0.4807108763317, false)
+        sendPacket(0.10408037809304, false)
+        sendPacket(0.0, true)
+        crit(entity)
+    }
+}
