433b553794fdb666c250f8d5d1dfe2c061c79b44
ManInMyVan <maninmyvan@gmail.com>
2024-03-10 16:56:26 -0600

    1132178

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/Matrix2.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speedmodes/other/Matrix2.kt
@@ -8,25 +8,36 @@ package net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.oth
 import net.ccbluex.liquidbounce.features.module.modules.movement.speedmodes.SpeedMode
 import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
 import net.ccbluex.liquidbounce.utils.MovementUtils.strafe
+import net.ccbluex.liquidbounce.utils.PacketUtils.sendPacket
 import net.ccbluex.liquidbounce.utils.extensions.jmp
+import net.minecraft.network.play.client.C0BPacketEntityAction
 
 /**
  * @author CCBlueX/LiquidBounce
+ * @author EclipsesDev
  */
-object Matrix : SpeedMode("Matrix") {
-
+object Matrix2 : SpeedMode("Matrix2") {
     override fun onUpdate() {
         if (mc.thePlayer.isInWater || mc.thePlayer.isInLava || mc.thePlayer.isInWeb || mc.thePlayer.isOnLadder)
             return
+
         if (isMoving) {
             if (mc.thePlayer.onGround) {
                 mc.thePlayer.jmp()
-                mc.thePlayer.speedInAir = 0.02098f
-                mc.timer.timerSpeed = 1.055f
-            } else
+                mc.timer.timerSpeed = 0.525f
                 strafe()
-        } else
-            mc.timer.timerSpeed = 1f
+
+                sendPacket(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.STOP_SPRINTING))
+            } else {
+                mc.timer.timerSpeed = 1.075f
+
+                sendPacket(C0BPacketEntityAction(mc.thePlayer, C0BPacketEntityAction.Action.START_SPRINTING))
+            }
+
+            mc.thePlayer.speedInAir = if (mc.thePlayer.fallDistance <= 0.8 && mc.thePlayer.moveStrafing == 0f)
+                0.02035f else 0.02f
+
+        } else mc.timer.timerSpeed = 1f
     }
 
     override fun onDisable() {
