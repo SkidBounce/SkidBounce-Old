bf4b48a60b72d51bbb9f1a6fe9999800f36f57ce
ManInMyVan <maninmyvan@gmail.com>
2024-03-18 20:15:25 -0600

    NoInvClose

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/NoInvClose.kt
@@ -0,0 +1,26 @@
+/*
+ * SkidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge, Forked from LiquidBounce.
+ * https://github.com/ManInMyVan/SkidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.misc
+
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory.MISC
+import net.ccbluex.liquidbounce.utils.ClientUtils.displayClientMessage
+import net.ccbluex.liquidbounce.value.BoolValue
+import net.minecraft.client.gui.inventory.GuiInventory
+import net.minecraft.network.play.server.S2EPacketCloseWindow
+
+object NoInvClose : Module("NoInvClose", MISC, gameDetecting = false) {
+    private val notify by BoolValue("Notify", true) // TODO: translations
+    @EventTarget
+    fun onPacket(event: PacketEvent) {
+        if (event.packet is S2EPacketCloseWindow && mc.currentScreen is GuiInventory) {
+            event.cancelEvent()
+            if (notify) displayClientMessage("§cThe server attempted to close your inventory.")
+        }
+    }
+}
