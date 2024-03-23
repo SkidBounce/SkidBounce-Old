3f042f20998a5830d532a831e4c28a4d81de8a56
ManInMyVan <maninmyvan@gmail.com>
2024-03-23 14:18:27 -0600

    port Twillight jesus from FDP

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/jesusmodes/other/Twillight.kt
@@ -0,0 +1,28 @@
+/*
+ * SkidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge, Forked from LiquidBounce.
+ * https://github.com/ManInMyVan/SkidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement.jesusmodes.other
+
+import net.ccbluex.liquidbounce.event.MoveEvent
+import net.ccbluex.liquidbounce.features.module.modules.movement.jesusmodes.JesusMode
+import net.ccbluex.liquidbounce.utils.MovementUtils.strafe
+import net.ccbluex.liquidbounce.utils.extensions.inLiquid
+
+object Twillight : JesusMode("Twillight", false) {
+    override fun onUpdate() {
+        if (mc.thePlayer.inLiquid) {
+            mc.thePlayer.motionX *= 1.04
+            mc.thePlayer.motionZ *= 1.04
+            strafe()
+        }
+    }
+
+    override fun onMove(event: MoveEvent) {
+        if (mc.thePlayer.inLiquid) {
+            event.y = 0.01
+            mc.thePlayer.motionY = 0.01
+        }
+    }
+}
