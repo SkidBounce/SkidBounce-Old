a042f462279e376b79a4680c255380fcd0f18cd1
ManInMyVan <maninmyvan@gmail.com>
2024-06-15 15:10:58 -0600

    e8e375d

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/Eagle.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/Eagle.kt
@@ -10,24 +10,43 @@ import net.ccbluex.liquidbounce.event.events.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory.PLAYER
 import net.ccbluex.liquidbounce.utils.block.BlockUtils.getBlock
+import net.ccbluex.liquidbounce.utils.extensions.getBlock
+import net.ccbluex.liquidbounce.utils.extensions.isActuallyPressed
+import net.ccbluex.liquidbounce.utils.timing.MSTimer
+import net.ccbluex.liquidbounce.value.BooleanValue
+import net.ccbluex.liquidbounce.value.FloatValue
+import net.ccbluex.liquidbounce.value.IntValue
 import net.minecraft.client.settings.GameSettings
 import net.minecraft.init.Blocks.air
 import net.minecraft.util.BlockPos
 
 object Eagle : Module("Eagle", PLAYER) {
 
+    private val sneakDelay by IntValue("SneakDelay", 0, 0..100)
+    private val onlyWhenLookingDown by BooleanValue("OnlyWhenLookingDown", false)
+    private val lookDownThreshold by FloatValue("LookDownThreshold", 45f, 0f..90f) { onlyWhenLookingDown }
+
+    private val sneakTimer = MSTimer()
+
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
-        val thePlayer = mc.thePlayer ?: return
+        mc.thePlayer ?: return
 
-        mc.gameSettings.keyBindSneak.pressed = mc.thePlayer.onGround && getBlock(BlockPos(thePlayer).down()) == air
+        if (mc.thePlayer.onGround
+            && BlockPos(mc.thePlayer).down().getBlock() == air
+            && (!onlyWhenLookingDown || mc.thePlayer.rotationPitch >= lookDownThreshold)
+            && sneakTimer.hasTimePassed(sneakDelay)
+        ) {
+            mc.gameSettings.keyBindSneak.pressed = true
+            sneakTimer.reset()
+        } else mc.gameSettings.keyBindSneak.pressed = mc.gameSettings.keyBindSneak.isActuallyPressed
     }
 
     override fun onDisable() {
         if (mc.thePlayer == null)
             return
 
-        if (!GameSettings.isKeyDown(mc.gameSettings.keyBindSneak))
+        if (!mc.gameSettings.keyBindSneak.isActuallyPressed)
             mc.gameSettings.keyBindSneak.pressed = false
     }
 }
