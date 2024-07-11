eba9fc0f0a6411473e0e387957188e282fa4e608
ManInMyVan <maninmyvan@gmail.com>
2024-07-10 20:30:12 -0600

    more strafe settings

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Strafe.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Strafe.kt
@@ -14,10 +14,12 @@ import net.ccbluex.liquidbounce.features.module.ModuleCategory.MOVEMENT
 import net.ccbluex.liquidbounce.utils.MovementUtils.direction
 import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
 import net.ccbluex.liquidbounce.utils.MovementUtils.speed
+import net.ccbluex.liquidbounce.utils.extensions.inLiquid
 import net.ccbluex.liquidbounce.utils.extensions.jmp
 import net.ccbluex.liquidbounce.utils.extensions.toDegreesF
 import net.ccbluex.liquidbounce.value.BooleanValue
 import net.ccbluex.liquidbounce.value.FloatValue
+import net.ccbluex.liquidbounce.value.IntValue
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -25,11 +27,17 @@ object Strafe : Module("Strafe", MOVEMENT, gameDetecting = false) {
 
     private val strength by FloatValue("Strength", 0.5F, 0F..1F)
     private val noMoveStop by BooleanValue("NoMoveStop", false)
-    private val onGroundStrafe by BooleanValue("OnGroundStrafe", false)
+
+    private val inAir by BooleanValue("InAir", true)
+    private val inLiquid by BooleanValue("InLiquid", true)
+    private val outOfLiquidTicks by IntValue("OutOfLiquidTicks", 40, 0..100)
+    private val onGround by BooleanValue("OnGroundStrafe", false)
+
     private val allDirectionsJump by BooleanValue("AllDirectionsJump", false)
 
     private var wasDown = false
     private var jump = false
+    private var ticksOutOfLiquid = 0
 
     @EventTarget
     fun onJump(event: JumpEvent) {
@@ -44,6 +52,10 @@ object Strafe : Module("Strafe", MOVEMENT, gameDetecting = false) {
 
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
+        if (mc.thePlayer.inLiquid) {
+            ticksOutOfLiquid = 0
+        } else ticksOutOfLiquid++
+
         if (mc.thePlayer.onGround && mc.gameSettings.keyBindJump.isKeyDown && allDirectionsJump && isMoving && !(mc.thePlayer.isInWater || mc.thePlayer.isInLava || mc.thePlayer.isOnLadder || mc.thePlayer.isInWeb)) {
             if (mc.gameSettings.keyBindJump.isKeyDown) {
                 mc.gameSettings.keyBindJump.pressed = false
@@ -67,21 +79,26 @@ object Strafe : Module("Strafe", MOVEMENT, gameDetecting = false) {
     fun onStrafe(event: StrafeEvent) {
         if (!isMoving) {
             if (noMoveStop) {
-                mc.thePlayer.motionX = .0
-                mc.thePlayer.motionZ = .0
+                mc.thePlayer.motionX = 0.0
+                mc.thePlayer.motionZ = 0.0
             }
             return
         }
 
-        val shotSpeed = speed
-        val speed = shotSpeed * strength
-        val motionX = mc.thePlayer.motionX * (1 - strength)
-        val motionZ = mc.thePlayer.motionZ * (1 - strength)
-
-        if (!mc.thePlayer.onGround || onGroundStrafe) {
+        if (shouldStrafe) {
+            val speed = speed * strength
             val yaw = direction
-            mc.thePlayer.motionX = -sin(yaw) * speed + motionX
-            mc.thePlayer.motionZ = cos(yaw) * speed + motionZ
+
+            mc.thePlayer.motionX = mc.thePlayer.motionX * (1 - strength) - sin(yaw) * speed
+            mc.thePlayer.motionZ = mc.thePlayer.motionZ * (1 - strength) + cos(yaw) * speed
         }
     }
+
+    private val shouldStrafe: Boolean
+        get() {
+            if (inLiquid && mc.thePlayer.inLiquid) return true
+            if (ticksOutOfLiquid <= outOfLiquidTicks) return true
+
+            return if (mc.thePlayer.onGround) onGround else inAir
+        }
 }
