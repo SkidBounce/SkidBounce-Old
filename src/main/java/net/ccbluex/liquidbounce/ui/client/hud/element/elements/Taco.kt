936c0d44d07749cbf8d41447c3f1111eb7204deb
ManInMyVan <maninmyvan@gmail.com>
2024-07-27 11:01:00 -0600

    Update Taco.kt

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Taco.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Taco.kt
@@ -1,3 +1,8 @@
+/*
+ * SkidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge, Forked from LiquidBounce.
+ * https://github.com/ManInMyVan/SkidBounce/
+ */
 package net.ccbluex.liquidbounce.ui.client.hud.element.elements
 
 import net.ccbluex.liquidbounce.features.command.commands.TacoCommand.toggle
